35f29568887e62ee5b345c380e38ee619bd1a871
hugojoensson <hugojoensson@gmail.com>
2024-02-17 12:50:09 +0100

    Lagt till MainMenu
    Lagt till MainMenu scen
    + Quit game tillagt
    + Play game tillagt
    - Options i menyn är trasigt

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public CanvasGroup Optionpanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Option()
+    {
+        Optionpanel.alpha = 1;
+        Optionpanel.blocksRaycasts = true;
+    }
+    public void Back()
+    {
+        Optionpanel.alpha = 0;
+        Optionpanel.blocksRaycasts = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
