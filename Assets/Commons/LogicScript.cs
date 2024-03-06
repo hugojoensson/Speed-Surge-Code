6e4eea3388dfc05bc0ae320af1f61c1e4e282fe4
hugojoensson <hugojoensson@gmail.com>
2024-03-06 16:56:09 +0100

    Fixat mindre saker
    + Lagt till near miss score på success menu på alla levlar.
    + Skapat likadant UI på alla levlar
    
    - Fixa collider brevid vägen på alla levlar.
    - Kanske lägga till ljud

--- Assets/Commons/LogicScript.cs
+++ Assets/Commons/LogicScript.cs
@@ -6,13 +6,22 @@ using UnityEngine.UI;
 public class LogicScript : MonoBehaviour
 {
     public int playerNearMiss;
+    public int playerNearMiss2;
     public Text inGameScoreText;
+    public Text inGameScoreText2;
 
+    void Start()
+    {
+        // playerNearMiss2 = playerNearMiss - 1;
+    }
 
     [ContextMenu("Öka Near misses")]
     public void  addNearMiss()
     {
         playerNearMiss = playerNearMiss + 1;
         inGameScoreText.text = playerNearMiss.ToString();
+
+        playerNearMiss2 = playerNearMiss2 + 1;
+        inGameScoreText2.text = playerNearMiss2.ToString();
     }
 }
